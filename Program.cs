b053c5c28dbe6ec32bf1a06b4b9faa6c7de2fc4d
hemajun815 <411388997@qq.com>
2017-03-13 21:07:59 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnonymousFunction
+{
+    class Program
+    {
+        delegate void del(string s);
+        static void Main()
+        {
+            del d = delegate(string str) { Console.WriteLine(str); };
+            d("匿名函数");
+            d += (str) => { Console.WriteLine(str.ToUpper()); };
+            d("匿名函数的Lamda表达式");
+            Console.ReadKey();
+        }
+    }
+}
