8e39f3974b05e060c1ff2455036d8bbae24cfb4e
hemajun815 <411388997@qq.com>
2017-09-11 17:08:34 +0800

    Update Program.cs

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -1,1 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace Interface
+{
+    class Program
+    {
+        static void Main()
+        {
+            WriteCode(new CSharpCoder());
+            WriteCode(new JavaCoder());
+            Console.ReadKey();
+        }
+
+        static private void WriteCode(ICoder coder)
+        {
+            coder.WriteCode();
+        }
+    }
+}
